4501a88dbde6384318eecf64c6a842c13d1c5c7f
Robbie Santos <robbiesantos@Robbies-MacBook-Pro.local>
2017-03-14 00:30:10 -0700

    Modify header.js to be a reusable component

--- index.ios.js
+++ index.ios.js
@@ -4,8 +4,8 @@ import React from 'react';
 import { AppRegistry } from 'react-native';
 import Header from './src/components/header';
 
-const App = () => (
-   <Header />
+const App = () => ( 
+   <Header headerText={'Albums'} />
 ); 
 
 AppRegistry.registerComponent('albums', () => App);
