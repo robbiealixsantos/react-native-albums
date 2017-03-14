4ac86da060481485d45d09502d8adb14c2b9ab20
Robbie Santos <robbiesantos@Robbies-MacBook-Pro.local>
2017-03-13 23:05:24 -0700

    Further header styling added to viewStyle prop

--- src/components/header.js
+++ src/components/header.js
@@ -15,7 +15,14 @@ const styles = {
 	viewStyle: {
 		backgroundColor: '#F8F8F8',
 		justifyContent: 'center',
-		alignItems: 'center'
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 15,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.5,
+		elevation: 2,
+		postition: 'relative'
 	},
 	textStyle: {
 		fontSize: 20
